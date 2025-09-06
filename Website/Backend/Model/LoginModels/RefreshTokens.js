183febbd52c46ba5a132a614ad10bc21da5e2486
XICO <xico9977@gmail.com>
2025-09-06 09:00:55 +0530

    doit

--- Website/Backend/Model/LoginModels/RefreshTokens.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const RefreshTokenSchema = new mongoose.Schema({
-    id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    Refreshtoken: { type: String, required: true }
-})
-
-export default mongoose.model("RefreshToken", RefreshTokenSchema);
